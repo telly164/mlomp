f1cc569bad27c78db517030283c0930cdc5e0aa8
telly164 <tellycode685@gmail.com>
2025-04-01 02:59:29 +0000

    sdfgvbhnj,;

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { componentTagger } from "lovable-tagger"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Laisse "/" pour un déploiement sur Vercel
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/mlomp-municipal-website/' : '/',
-  server: {
-    port: 8080,
-    host: "::"
-  }
-}))
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+}));
